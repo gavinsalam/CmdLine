020c713be45ba5c98e0fe5ac5de2e1e7a86e84e7
Gavin Salam <gavin.salam@cern.ch>
2007-02-03 17:34:06 +0000

    added various standard files and some proper headers to files
    git-svn-id: svn+ssh://svn.lpthe.jussieu.fr/home/svn/CmdLine/trunk@9 1ad09da8-d3d1-4499-9b68-dc0d2c64ceb7

--- CmdLine.cc
+++ CmdLine.cc
@@ -1,32 +1,27 @@
-//STARTHEADER
-// $Id: CmdLine.cc 293 2006-08-17 19:38:38Z salam $
-//
-// Copyright (c) 2005 Matteo Cacciari and Gavin Salam
-//
-//----------------------------------------------------------------------
-// This file is part of a simple command-line handling environment
-//
-//  FastJet is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  The algorithms that underlie FastJet have required considerable
-//  development and are described in hep-ph/0512210. If you use
-//  FastJet as part of work towards a scientific publication, please
-//  include a citation to the FastJet paper.
-//
-//  FastJet is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with FastJet; if not, write to the Free Software
-//  Foundation, Inc.:
-//      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//----------------------------------------------------------------------
-//ENDHEADER
+///////////////////////////////////////////////////////////////////////////////
+// File: CmdLine.cc                                                          //
+// Part of the CmdLine library                                               //
+//                                                                           //
+// Copyright (c) 2007 Gavin Salam                                            //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify      //
+// it under the terms of the GNU General Public License as published by      //
+// the Free Software Foundation; either version 2 of the License, or         //
+// (at your option) any later version.                                       //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+// You should have received a copy of the GNU General Public License         //
+// along with this program; if not, write to the Free Software               //
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA //
+//                                                                           //
+// $Revision:: 139                                                          $//
+// $Date:: 2007-01-23 16:09:23 +0100 (Tue, 23 Jan 2007)                     $//
+///////////////////////////////////////////////////////////////////////////////
+
 
 
 #include "CmdLine.hh"
